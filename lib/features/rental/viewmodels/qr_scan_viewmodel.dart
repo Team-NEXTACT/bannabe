79e98803cf3884d50fc7f6c0449768937a3b4758
Syeong <pyqw_@naver.com>
2025-04-03 15:30:46 +0900

    refactor: 불필요한 코드 제거
    - QR 스캔 뷰에서 디버깅용 스킵 버튼 및 mock 데이터 제거
    - QRScanViewModel에서 불필요한 코드 제거
    - 사용하지 않는 AccessoryRepository 파일 삭제

--- lib/features/rental/viewmodels/qr_scan_viewmodel.dart
+++ lib/features/rental/viewmodels/qr_scan_viewmodel.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:permission_handler/permission_handler.dart';
 import '../../../data/models/rental.dart';
-import '../../../data/repositories/accessory_repository.dart';
-import '../../../data/repositories/station_repository.dart';
 import '../../../core/services/api_service.dart';
 import 'package:dio/dio.dart';
 
@@ -27,7 +25,6 @@ class RentalItemResponse {
 }
 
 class QRScanViewModel extends ChangeNotifier {
-  final AccessoryRepository _accessoryRepository;
   final int _rentalDuration;
   final bool isReturn;
   final dynamic initialRental;
@@ -35,18 +32,13 @@ class QRScanViewModel extends ChangeNotifier {
   bool _hasCameraPermission = false;
   String? _error;
   Rental? _rental;
-  bool _isReturnComplete = false;
-  int _rating = 0;
-  final _stationRepository = StationRepository.instance;
   RentalItemResponse? _rentalItemResponse;
 
   QRScanViewModel({
-    AccessoryRepository? accessoryRepository,
     required int rentalDuration,
     required this.isReturn,
     this.initialRental,
-  })  : _accessoryRepository = accessoryRepository ?? AccessoryRepository(),
-        _rentalDuration = rentalDuration {
+  }) : _rentalDuration = rentalDuration {
     _checkCameraPermission();
   }
 
@@ -54,8 +46,6 @@ class QRScanViewModel extends ChangeNotifier {
   bool get hasCameraPermission => _hasCameraPermission;
   String? get error => _error;
   Rental? get rental => _rental;
-  bool get isReturnComplete => _isReturnComplete;
-  int get rating => _rating;
   RentalItemResponse? get rentalItemResponse => _rentalItemResponse;
 
   Future<void> _checkCameraPermission() async {
@@ -86,15 +76,12 @@ class QRScanViewModel extends ChangeNotifier {
     notifyListeners();
 
     try {
-      // QR 코드에서 URL 확인
       if (!qrCode.startsWith('https://api.bannabe.io/rentals/')) {
         throw Exception('유효하지 않은 QR 코드입니다.');
       }
 
-      // URL에서 토큰 추출
       final rentalItemToken = qrCode.split('/').last;
 
-      // API 호출 시 validateStatus 추가
       final response = await ApiService.instance.get(
         '/rentals/$rentalItemToken',
         options: Options(validateStatus: (status) => status! < 500),
@@ -141,10 +128,6 @@ class QRScanViewModel extends ChangeNotifier {
     notifyListeners();
 
     try {
-      // QR 코드에서 스테이션 ID 추출
-      final stationId = qrCode.split('_')[0];
-
-      // 반납 처리
       final now = DateTime.now();
       _rental = Rental(
         name: initialRental!.name,
@@ -154,7 +137,6 @@ class QRScanViewModel extends ChangeNotifier {
         expectedReturnTime: initialRental!.expectedReturnTime,
         token: initialRental!.token,
       );
-      _isReturnComplete = true;
     } catch (e) {
       _error = e.toString();
     } finally {
